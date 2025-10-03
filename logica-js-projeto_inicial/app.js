b289c0d4bb40d06da2f682c519402017ead9afd2
Gustavo de Paula <gdepaula@gmail.com>
2025-10-03 14:11:05 -0300

    Else e comentários

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -5,4 +5,6 @@ let chute = prompt('Escolha um número entre 1 e 30');
 
 if (numeroSecreto === chute) {
   console.log('isso ai vc acertou o número secreto');
+} else {
+  console.log('Vc errou');
 }
