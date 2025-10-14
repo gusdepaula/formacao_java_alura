104e7f833a6869328a23019592eca5bff3d731f9
Gustavo de Paula <gdepaula@gmail.com>
2025-10-14 15:54:34 -0300

    Atribuindo valores ao nosso filme

---
+++ ScreenMatch/src/Principal.java
@@ -0,0 +1,11 @@
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O Poderoso Chefão";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+
+        System.out.println(meuFilme.nome);
+    }
+}
