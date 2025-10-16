5325f789ba5ade67bc73f648c78ca2a8d0009755
Gustavo de Paula <gdepaula@gmail.com>
2025-10-16 08:36:13 -0300

    Visualizando característcas privadas

--- ScreenMatch/src/Principal.java
+++ ScreenMatch/src/Principal.java
@@ -10,14 +10,12 @@ public class Principal {
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-
-        System.out.println(meuFilme.somaAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
 
-        meuFilme.somaAvaliacoes = 10;
-        meuFilme.totalDeAvaliacoes = 1;
+//        meuFilme.somaAvaliacoes = 10;
+//        meuFilme.totalDeAvaliacoes = 1;
 
-        System.out.println(meuFilme.pegaMedia());
+//        System.out.println(meuFilme.pegaMedia());
     }
 }
