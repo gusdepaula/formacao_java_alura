0b711736cc65e03c5e349d006f9495ec7492b4c0
Gustavo de Paula <gdepaula@gmail.com>
2025-10-27 08:14:01 -0300

    Ordenando a lista

--- ScreenMatch/src/principal/PrincipalComListas.java
+++ ScreenMatch/src/principal/PrincipalComListas.java
@@ -5,6 +5,7 @@ import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -31,5 +32,19 @@ public class PrincipalComListas {
                 System.out.println(item.getNome() + " não é um filme ou a classificação é menor ou igual a 2!" );
             }
         }
+
+        ArrayList<String> buscaPorArtista = new ArrayList<>();
+        buscaPorArtista.add("Adam Sandler");
+        buscaPorArtista.add("Denzel Washington");
+        buscaPorArtista.add("Sandra Bullock");
+        buscaPorArtista.add("Cameron Diaz");
+        System.out.println("Artista no índice 0: " + buscaPorArtista.get(0));
+
+        Collections.sort(buscaPorArtista);
+        System.out.println("Lista de artistas ordenada: " + buscaPorArtista);
+
+        Collections.sort(lista);
+        System.out.println("Lista de títulos ordenada: ");
+        System.out.println(lista);
     }
 }
