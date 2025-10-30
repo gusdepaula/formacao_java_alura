b54e7b99596d707bc88d34c5538faf939904fff7
Gustavo de Paula <gdepaula@gmail.com>
2025-10-30 13:49:43 -0300

    Refinando a busca por filmes

--- ScreenMatch/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ ScreenMatch/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -4,13 +4,19 @@ import java.io.IOException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
-        System.out.println("Bem-vindo ao Screen Match!");
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca = leitura.nextLine();
+        String buscaFormatada = busca.replace(" ", "+");
+        String endereco = "https://www.omdbapi.com/?t=" + buscaFormatada + "&apikey=5f672b50";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(java.net.URI.create("https://www.omdbapi.com/?t=The+Matrix&apikey=5f672b50"))
+                .uri(java.net.URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
