102d10797404f2ec8f0a7deedad9a62e4a2b4495
Gustavo de Paula <gdepaula@gmail.com>
2025-11-03 09:41:25 -0300

    Modelando dados da sérieM

--- screenmatchspring/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatchspring/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch;
 
 import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverterDados;
+import br.com.alura.screenmatch.model.DadosSerie;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,14 +10,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenmatchApplication.class, args);
+    }
 
     @Override
     public void run(String... args) throws Exception {
         var consumoAPI = new ConsumoAPI();
-        //var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=lost&Season=1&apikey=5f672b50");
+        var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=lost&Season=1&apikey=5f672b50");
         System.out.println(json);
+        ConverterDados conversor = new ConverterDados();
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
     }
 }
