d77e7240ebe67df35bd9274a62f7be00a6bcf94c
Gustavo de Paula <gdepaula@gmail.com>
2026-01-08 12:16:25 -0300

    Representando séries do banco

--- java-screenmatch-web/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ java-screenmatch-web/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,12 +1,13 @@
 package br.com.alura.screenmatch.controller;
 
-import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.dto.SerieDTO;
 import br.com.alura.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
@@ -15,7 +16,19 @@ public class SerieController {
     private SerieRepository repositorio;
 
     @GetMapping("/series")
-    public List<Serie> obterSeries() {
-        return repositorio.findAll();
+    public List<SerieDTO> obterSeries() {
+        return repositorio.findAll()
+                .stream()
+                .map(serie -> new SerieDTO(
+                        serie.getId(),
+                        serie.getTitulo(),
+                        serie.getTotalTemporadas(),
+                        serie.getAvaliacao(),
+                        serie.getGenero(),
+                        serie.getAtores(),
+                        serie.getPoster(),
+                        serie.getSinopse()))
+                .collect(Collectors.toList());
     }
+
 }
