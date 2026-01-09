c18f06387ca771d44e1577d46db5c6f574d1ebad
Gustavo de Paula <gdepaula@gmail.com>
2026-01-09 09:08:25 -0300

    Retornando os detalhes da série

--- java-screenmatch-web/src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ java-screenmatch-web/src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -37,5 +38,27 @@ public class SerieService {
                         serie.getPoster(),
                         serie.getSinopse()))
                 .collect(Collectors.toList());
+    }
+
+    public List<SerieDTO> obterLancamentos() {
+        return converteDados(repositorio.findTop5ByOrderByEpisodiosDataLancamentoDesc());
+    }
+
+    public SerieDTO obtePorId(Long id) {
+        Optional<Serie> serie = repositorio.findById(id);
+        if (serie.isPresent()) {
+            Serie s = serie.get();
+            return new SerieDTO(
+                    s.getId(),
+                    s.getTitulo(),
+                    s.getTotalTemporadas(),
+                    s.getAvaliacao(),
+                    s.getGenero(),
+                    s.getAtores(),
+                    s.getPoster(),
+                    s.getSinopse()
+            );
+        }
+        return null;
     }
 }
