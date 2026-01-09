af60f11d33032a306120f00853d3b6369bba6b36
Gustavo de Paula <gdepaula@gmail.com>
2026-01-09 14:35:33 -0300

    Ajustando a busca de lançamentos

--- java-screenmatch-web/src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ java-screenmatch-web/src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -41,7 +41,7 @@ public class SerieService {
     }
 
     public List<SerieDTO> obterLancamentos() {
-        return converteDados(repositorio.findTop5ByOrderByEpisodiosDataLancamentoDesc());
+        return converteDados(repositorio.lancamentosMaisRecentes());
     }
 
     public SerieDTO obtePorId(Long id) {
