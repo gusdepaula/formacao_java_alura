5608680062eafb1e55678a03e2b997b79a172d36
Gustavo de Paula <gdepaula@gmail.com>
2026-01-09 08:31:20 -0300

    Criando um servi;co para séries

--- java-screenmatch-web/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ java-screenmatch-web/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,7 +1,7 @@
 package br.com.alura.screenmatch.controller;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
-import br.com.alura.screenmatch.repository.SerieRepository;
+import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,29 +11,12 @@ import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
-
     @Autowired
-    private SerieRepository repositorio;
+    private SerieService servico;
 
     @GetMapping("/series")
     public List<SerieDTO> obterSeries() {
-        return repositorio.findAll()
-                .stream()
-                .map(serie -> new SerieDTO(
-                        serie.getId(),
-                        serie.getTitulo(),
-                        serie.getTotalTemporadas(),
-                        serie.getAvaliacao(),
-                        serie.getGenero(),
-                        serie.getAtores(),
-                        serie.getPoster(),
-                        serie.getSinopse()))
-                .collect(Collectors.toList());
-    }
-
-    @GetMapping("/inicio")
-    public String inicio() {
-        return "Bem-vindo ao Screenmatch!";
+        return servico.obterTodasAsSeries();
     }
 
 }
---
+++ java-screenmatch-web/src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -0,0 +1,32 @@
+package br.com.alura.screenmatch.service;
+
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService {
+
+
+    @Autowired
+    private SerieRepository repositorio;
+
+    public List<SerieDTO> obterTodasAsSeries() {
+        return repositorio.findAll()
+                .stream()
+                .map(serie -> new SerieDTO(
+                        serie.getId(),
+                        serie.getTitulo(),
+                        serie.getTotalTemporadas(),
+                        serie.getAvaliacao(),
+                        serie.getGenero(),
+                        serie.getAtores(),
+                        serie.getPoster(),
+                        serie.getSinopse()))
+                .collect(Collectors.toList());
+    }
+}
